2b9d5954f6cc70fe82856dc026199882d2cedc33
Thundergod <81786979+almightygodthor@users.noreply.github.com>
2026-09-26 02:02:59 +0530

    Update vendorsetup.sh with build environment settings
    Added build environment settings and increased open-file limit.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
+# Build environment settings
+unset CUSTOM_NINJA_BIN
+
+# Increase open-file limit for recovery/older Android builds
+ulimit -n 65535
+
 # Lunch combos are declared in AndroidProducts.mk.
 export FOX_INSTALLER_VENDOR_BOOT_RAMDISK_INSTALL=0
 export FOX_DELETE_AROMAFM=1
